a87d421d677da28e511aaf4e9f261126ef68ada1
Tareq <tmasri58@gmail.com>
2019-09-06 21:55:58 -0400

    user is not able to register, generate a jwt token, and login

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Routes
 const UserRoute = require('./routes/users');
 const AuthRoute = require('./routes/auth');
 const ContactRoute = require('./routes/contacts');
 
-const app = express();
 
 app.get('/', (req, res) => 
    res.json({ msg: 'Welcome to the contact keeper API' })
