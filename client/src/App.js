cb22bf97c3265ffb371924b38eb884dd61de98bb
Tareq <tmasri58@gmail.com>
2019-09-07 20:50:32 -0400

    built a basic UI with a navbar and contacts, added a few contacts to test the UI with

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './App.css'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+
+import ContactState from './context/contact/ContactState'
 
 const App = () => {
   return (
-    <div className="App">
-      My App
-    </div>
-  );
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  )
 }
 
-export default App;
+export default App
